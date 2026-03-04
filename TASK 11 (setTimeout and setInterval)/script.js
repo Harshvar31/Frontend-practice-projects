409092faf55d3581e65c256ff48e6d3adbc1663f
Harshvardhan Pandey <nakulpandey430@gmail.com>
2026-03-04 23:17:15 +0530

    Added TASK 11 (setTimeout and setInterval)

---
+++ TASK 11 (setTimeout and setInterval)/script.js
@@ -0,0 +1,26 @@
+let grow = 0
+let btn = document.querySelector('button');
+let h2 = document.querySelector('h2');
+let inner = document.querySelector('#inner');
+
+btn.addEventListener('click', function(){
+    
+    btn.style.pointerEvents = 'none'
+
+    let num = 50 + Math.floor(Math.random()*50)
+
+    console.log('Your file will be downloaded in',num/10,'seconds');
+
+    let int = setInterval(() => {
+        grow++
+        h2.innerHTML = grow+'%'
+        inner.style.width = grow+'%'
+    }, num);
+
+    setTimeout(() => {
+        clearInterval(int);
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5
+    }, num*100);
+    
+})
